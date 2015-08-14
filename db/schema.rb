9a102d961a06577451248b1d16aa03bb7870eaec
Lance Vincent <lancevincent@gmail.com>
2015-08-14 18:40:18 +0800

    Added movie scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814101746) do
+ActiveRecord::Schema.define(version: 20150814102822) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "movie_length"
+    t.string   "director"
+    t.string   "rating"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
