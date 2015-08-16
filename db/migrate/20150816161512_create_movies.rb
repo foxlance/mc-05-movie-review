2e165bce06756dfa110571c19f6e8a407a05aade
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-08-17 00:16:37 +0800

    added movie controller and model

---
+++ db/migrate/20150816161512_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :movie_length
+      t.string :director
+      t.string :rating
+      t.belongs_to :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816160618) do
+ActiveRecord::Schema.define(version: 20150816161512) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "movie_length"
+    t.string   "director"
+    t.string   "rating"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "movies", ["user_id"], name: "index_movies_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
