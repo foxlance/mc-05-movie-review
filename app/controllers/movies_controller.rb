e3782eaa094b567acf2a05e5841d84a568ed234d
Lance Vincent <lancevincent@gmail.com>
2015-08-18 19:59:45 +0800

    initial part for movie CRUD

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,22 @@
 class MoviesController < ApplicationController
+	before_action :set_movie, only: [:edit, :update, :show, :destroy]
 
 	def index
+		@movies = Movie.all().order('created_at DESC')	
+	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
 		
+	end
+
+	private
+
+	def set_movie
+		@movie = Movie.find(params[:id])
 	end
 
 end
